7c12dc1392b4be2f2376d18baf2c6ea3a391b7cc
KnezoWTF <206253445+knezoWTF@users.noreply.github.com>
2025-08-28 19:18:15 +0200

    Feather support

--- src/main/java/com/volt/utils/mc/ChatUtils.java
+++ src/main/java/com/volt/utils/mc/ChatUtils.java
@@ -7,6 +7,11 @@ import net.minecraft.text.Text;
 @UtilityClass
 public final class ChatUtils implements IMinecraft {
     public static void addChatMessage(String text) {
+        if (mc.player == null || mc.world == null || mc.inGameHud == null || mc.inGameHud.getChatHud() == null) {
+            System.out.println("[Volt] " + text);
+            return;
+        }
         mc.inGameHud.getChatHud().addMessage(Text.of(text));
     }
+
 }
