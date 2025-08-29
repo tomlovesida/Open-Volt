aba473957da62941027b34d74f6a1ab8cac555a6
KnezoWTF <206253445+knezoWTF@users.noreply.github.com>
2025-08-29 19:35:11 +0200

    Pasted? I will not speak without a lawyer

---
+++ src/main/java/com/volt/event/impl/player/PreMotion.java
@@ -0,0 +1,57 @@
+package com.volt.event.impl.player;
+
+import com.volt.IMinecraft;
+import com.volt.event.types.Event;
+import lombok.Getter;
+import lombok.Setter;
+import net.minecraft.util.math.MathHelper;
+
+@Getter
+public class PreMotion implements Event, IMinecraft {
+
+    private final double posX;
+    private final double posY;
+    private final double posZ;
+
+    @Setter
+    private float yaw;
+    @Setter
+    private float pitch;
+    private final float lastYaw;
+    private final float lastPitch;
+
+    private final boolean onGround;
+    private final boolean isSprinting;
+    private final boolean isSneaking;
+
+    @Setter
+    private boolean pitchChanged = false;
+
+    public PreMotion(double posX, double posY, double posZ,
+                     float yaw, float pitch,
+                     float lastYaw, float lastPitch,
+                     boolean onGround, boolean isSprinting, boolean isSneaking) {
+        this.posX = posX;
+        this.posY = posY;
+        this.posZ = posZ;
+        this.yaw = yaw;
+        this.pitch = pitch;
+        this.lastYaw = lastYaw;
+        this.lastPitch = lastPitch;
+        this.onGround = onGround;
+        this.isSprinting = isSprinting;
+        this.isSneaking = isSneaking;
+    }
+
+    public void turnHead() {
+        assert mc.player != null;
+        float f = MathHelper.wrapDegrees(yaw - mc.player.bodyYaw);
+        mc.player.bodyYaw += f * 0.3F;
+        float h = 50.0f;
+        if (Math.abs(f) > h) {
+            mc.player.bodyYaw += f - (float) MathHelper.sign(f) * h;
+        }
+
+        mc.player.headYaw = yaw;
+    }
+}
---
+++ src/main/java/com/volt/mixin/ClientPlayerEntityMixin.java
@@ -0,0 +1,137 @@
+package com.volt.mixin;
+
+import com.mojang.authlib.GameProfile;
+import com.volt.Volt;
+import com.volt.event.impl.player.PreMotion;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(ClientPlayerEntity.class)
+public abstract class ClientPlayerEntityMixin extends AbstractClientPlayerEntity {
+
+    @Shadow
+    protected abstract void sendSprintingPacket();
+
+    @Shadow
+    protected abstract boolean isCamera();
+
+    @Shadow
+    private double lastX;
+
+    @Shadow
+    private double lastBaseY;
+
+    @Shadow
+    private double lastZ;
+
+    @Shadow
+    private float lastYaw;
+
+    @Shadow private float lastPitch;
+
+    @Shadow
+    private int ticksSinceLastPositionPacketSent;
+
+    @Shadow
+    @Final
+    public ClientPlayNetworkHandler networkHandler;
+
+    @Shadow
+    private boolean lastOnGround;
+
+    @Shadow
+    private boolean autoJumpEnabled;
+    @Shadow
+    @Final
+    protected MinecraftClient client;
+
+    public ClientPlayerEntityMixin(ClientWorld world, GameProfile profile) {
+        super(world, profile);
+    }
+
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    private void sendMovementPackets() {
+        PreMotion event = new PreMotion(
+                this.getX(),
+                this.getBoundingBox().minY,
+                this.getZ(),
+                this.getYaw(),
+                this.getPitch(),
+                this.lastYaw,
+                this.lastPitch,
+                this.isOnGround(),
+                this.isSprinting(),
+                this.isSneaking()
+        );
+
+
+        this.sendSprintingPacket();
+
+        if (this.isCamera()) {
+            Volt.INSTANCE.getVoltEventBus().post(event);
+            event.setPitchChanged(event.getPitch() != getPitch());
+
+            double dx = event.getPosX() - this.lastX;
+            double dy = event.getPosY() - this.lastBaseY;
+            double dz = event.getPosZ() - this.lastZ;
+            double dyaw = event.getYaw() - this.lastYaw;
+            double dpitch = event.getPitch() - this.lastPitch;
+
+            ++this.ticksSinceLastPositionPacketSent;
+
+            boolean moved = MathHelper.squaredMagnitude(dx, dy, dz) > MathHelper.square(2.0E-4) || this.ticksSinceLastPositionPacketSent >= 20;
+            boolean rotated = dyaw != 0.0 || dpitch != 0.0;
+
+            if (moved && rotated) {
+                this.networkHandler.sendPacket(new PlayerMoveC2SPacket.Full(
+                        event.getPosX(), event.getPosY(), event.getPosZ(),
+                        event.getYaw(), event.getPitch(),
+                        event.isOnGround()
+                ));
+            } else if (moved) {
+                this.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(
+                        event.getPosX(), event.getPosY(), event.getPosZ(),
+                        event.isOnGround()
+                ));
+            } else if (rotated) {
+                this.networkHandler.sendPacket(new PlayerMoveC2SPacket.LookAndOnGround(
+                        event.getYaw(), event.getPitch(),
+                        event.isOnGround()
+                ));
+            } else if (this.lastOnGround != event.isOnGround()) {
+                this.networkHandler.sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(
+                        event.isOnGround()
+                ));
+            }
+
+            if (moved) {
+                this.lastX = event.getPosX();
+                this.lastBaseY = event.getPosY();
+                this.lastZ = event.getPosZ();
+                this.ticksSinceLastPositionPacketSent = 0;
+            }
+
+            if (rotated) {
+                this.lastYaw = event.getYaw();
+                this.lastPitch = event.getPitch();
+            }
+
+            this.lastOnGround = event.isOnGround();
+            this.autoJumpEnabled = this.client.options.getAutoJump().getValue();
+        }
+    }
+}
+
